675c88719242ebbdab44f8249f7db00a67e892b3
hale1017 <103413131+hale1017@users.noreply.github.com>
2022-04-11 20:51:38 +0800

    Add files via upload

---
+++ stanCode_projects/karel/StepUp.py
@@ -0,0 +1,59 @@
+"""
+Name: Jerry
+File: StepUp.py
+---------------------
+Pre-condition: Karel was at (1, 1), beeper was at (1, 2)
+Post-condition: Karel is at (2, 5), 99 beepers are at (2, 4)
+"""
+from karel.stanfordkarel import *
+
+
+def main():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
